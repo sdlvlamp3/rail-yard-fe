0d6ba81073c0e70e4c7d3cb1c7b7c640584c570c
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-29 21:18:03 -0500

    adding form for login. Will need authentication service to complete form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  isLoading=false
+
+  loginForm: FormGroup=new FormGroup({
+    username:new FormControl('', Validators.required),
+    password:new FormControl('', Validators.required),
+
+      })
+
+
+  isError:boolean=false
+
+
+
+//  constructor(private authService:AuthenticationService, private router:Router){}
+
+ login(){
+   if(this.loginForm.valid){
+     const username=this.loginForm.value.username;
+     const password=this.loginForm.value.password;
+     this.isLoading=true
+
+    //  this.authService.login(username, password).subscribe({
+    //    next: (res:any)=>{
+    //    this.router.navigate(['/'])
+    //    },
+    //    error: (error:any) =>{
+    //      console.log("Error when logging in", error )
+    //      this.isError=true
+    //      this.isLoading=false
+    //    }
+    //  })
+   }
+ }
+
+
+close(){
+  // this.router.navigate(['home']);
+}
+
 
 }
