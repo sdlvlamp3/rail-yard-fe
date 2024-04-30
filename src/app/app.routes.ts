cf6e6bb233d50c6a7f78ec64ee3f986237385c9d
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-30 14:32:58 -0500

    Setting up home landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
-  {path: 'login', component:LoginComponent}
+  { path: 'home', component: HomeComponent},
+  {path: 'login', component:LoginComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
