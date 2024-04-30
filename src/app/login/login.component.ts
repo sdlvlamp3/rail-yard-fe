62d298db5b3a8775599af43c4ea701feed50de5a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-30 10:38:05 -0500

    setting up back route will have to add auth service to constructor when I have it

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent {
 
 
 //  constructor(private authService:AuthenticationService, private router:Router){}
+ constructor(private router:Router){}
 
  login(){
    if(this.loginForm.valid){
@@ -48,7 +49,7 @@ export class LoginComponent {
 
 
 close(){
-  // this.router.navigate(['home']);
+  this.router.navigate(['/']);
 }
 
 
