910c9d9e1e1be22b3849e0fa7b33e1ee839a7b2d
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-01 08:47:02 -0500

    setting up cards and icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
