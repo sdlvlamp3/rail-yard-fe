e469a0bf4a7f4f6e531886c8b1ceebccbdd55cbb
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2024-05-05 13:08:28 -0500

    adding models

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,9 +1,12 @@
 export class User {
     constructor(
       id: number,
+      username: string,
       first_name: string,
       last_name: string,
       email: string,
       token: any,
-      role: string) { }
+      role: string
+    ){}
+    
   }
