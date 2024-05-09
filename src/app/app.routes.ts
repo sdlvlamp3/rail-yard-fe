bc335b120e7b93758dacaf7f24b310c903d6f85b
Brian Tuohy <BTcodez22@gmail.com>
2024-05-09 18:15:00 -0500

    update file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './core/components/landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './core/components/home-page/home-page.component';
-import { OrdersComponent } from './orders/orders.component';
+import { OrdersComponent } from './core/components/orders/orders.component';
 
 
 export const routes: Routes = [
@@ -25,3 +25,10 @@ export const routes: Routes = [
   loadComponent: () => import('./core/components/home-page/home-page.component')
     .then((m) => m.HomePageComponent)
   },
+
+  {
+    path: 'orders',
+    loadComponent: () => import('./core/components/orders/orders.component')
+      .then((m) => m.OrdersComponent)
+  }
+]
