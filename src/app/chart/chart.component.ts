2408df37b90c3dcfbad027279178cee714626f6b
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-16 19:25:58 -0500

    added chart npms and some logic

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.css'
 })
 export class ChartComponent {
+  public chartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public chartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartType: ChartType = 'bar';
+  public chartLegend = true;
+  public chartPlugins = [];
+
+  public chartData: ChartDataset[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+//need to check on this and errors?#@
+  // bootstrapApplication(AppComponent, {
+  //   providers: [provideCharts(withDefaultRegisterables())],
+  // }).catch((err) => console.error(err));
