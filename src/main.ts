bba898c61c1bf9479aa601145f0491ca0d0fad74
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-16 19:40:12 -0500

    chart is set up

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,23 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
-bootstrapApplication(AppComponent, appConfig)
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+
+//   bootstrapApplication(AppComponent, {
+//     providers: [provideCharts(withDefaultRegisterables())],
+//   }).catch((err) => console.error(err));
+
+
+
+const mergedConfig = {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideCharts(withDefaultRegisterables()),
+  ],
+};
+
+bootstrapApplication(AppComponent, mergedConfig)
   .catch((err) => console.error(err));
-
-
-//need to check on this and errors?#@
-  // bootstrapApplication(AppComponent, {
-  //   providers: [provideCharts(withDefaultRegisterables())],
-  // }).catch((err) => console.error(err));
