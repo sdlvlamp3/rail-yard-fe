947e1f3a538b1aaed853e736d82cc9bf7f1f48a7
Brian Tuohy <BTcodez22@gmail.com>
2024-05-20 16:59:47 -0500

    added delete function to order stats 'Released', began implementing a warning modal prior to deletion

--- src/app/core/components/orders/orders.component.ts
+++ src/app/core/components/orders/orders.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule,
+  MatMenuTrigger
+} from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { Order } from '../../models/order';
 import { OrdersService } from '../../services/orders.service';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-orders',
@@ -24,7 +35,14 @@ import { Router } from '@angular/router';
 export class OrdersComponent implements OnInit{
   orderData: Order[] = [];
 
-  constructor(private ordersService: OrdersService, public router: Router) {}
+
+  constructor(
+    private ordersService: OrdersService,
+    public router: Router,
+    
+  ) {}
+
+
 
   ngOnInit(): void {
       this.orderLoad()
@@ -44,6 +62,25 @@ export class OrdersComponent implements OnInit{
     });
 }
 
+
+//Order control logic lives here; modals, methods, and more
+
+openDelDialogue() {
+
+}
+
+closeDelDialogue() {
+
+}
+
+delOrder(order_id: number, event: MouseEvent) {
+  //event.stopPropagation()
+  this.ordersService.deleteOrder(order_id).subscribe(() => {
+    this.orderLoad()
+  });
+}
+
+  // Table logic lives here;
 
   displayedColumns: string[] = ['carId', 'requestedDate', 'receivedDate', 'extractionStart', 'extractionEnd', 'releaseDate', 'rawMaterialId', 'weight', 'actions'];
   dataSource = this.orderData;
